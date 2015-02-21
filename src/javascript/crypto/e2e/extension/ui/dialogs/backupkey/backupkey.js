456ead7092f55ffe583e89f0f0ec1fee368fe5e4
yan <yzhu@yahoo-inc.com>
2015-02-20 17:11:51 -0800

    Start adding phrase backup codes

--- src/javascript/crypto/e2e/extension/ui/dialogs/backupkey/backupkey.js
+++ src/javascript/crypto/e2e/extension/ui/dialogs/backupkey/backupkey.js
@@ -26,6 +26,7 @@ goog.require('e2e.ext.actions.Executor');
 goog.require('e2e.ext.constants.Actions');
 goog.require('e2e.ext.ui.dialogs.Overlay');
 goog.require('e2e.ext.ui.templates.dialogs.backupkey');
+goog.require('e2e.ext.PhraseGenerator');
 goog.require('goog.array');
 goog.require('goog.crypt.base64');
 goog.require('soy');
@@ -60,11 +61,10 @@ dialogs.BackupKey.prototype.createDom = function() {
 dialogs.BackupKey.prototype.decorateInternal = function(elem) {
   goog.base(this, 'decorateInternal', elem);
   this.setTitle(chrome.i18n.getMessage('keyMgmtBackupKeyringLabel'));
-  this.getBackupCode_().addCallback(goog.bind(function(key) {
+  this.getBackupPhrase_().addCallback(goog.bind(function(phrase) {
     soy.renderElement(this.getContentElement(), templates.backupKey, {
-      key: key,
-      caseSensitiveText:
-          chrome.i18n.getMessage('keyMgmtBackupKeyringCaseSensitive')
+      key: phrase,
+      caseSensitiveText: chrome.i18n.getMessage('keyMgmtBackupKeyringWarning')
     });
   }, this));
 };
@@ -89,4 +89,22 @@ dialogs.BackupKey.prototype.getBackupCode_ = function() {
   });
   return result;
 };
+
+
+/**
+ * Returns the backup code to display in the UI as a series of words.
+ * @private
+ * @return {e2e.async.Result.<string>} Words to display
+ */
+dialogs.BackupKey.prototype.getBackupPhrase_ = function() {
+  var result = new e2e.async.Result();
+  new e2e.ext.actions.Executor().execute(/** @type {!messages.ApiRequest} */ ({
+    action: constants.Actions.GET_KEYRING_BACKUP_DATA
+  }), this, /** @param {e2e.openpgp.KeyringBackupInfo} data */ function(data) {
+    // For now we only allow one keypair per ECC seed, so data.count is fixed
+    result.callback(e2e.ext.PhraseGenerator.bytesToPhrase(data.seed));
+  });
+  return result;
+};
+
 });  // goog.scope
