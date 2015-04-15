60ed97df8cbff87275579ce28f84210f71d6f8bc
yan <yzhu@yahoo-inc.com>
2015-04-15 09:35:42 -0700

    Restore config options for internal release

--- src/javascript/crypto/e2e/extension/config.js
+++ src/javascript/crypto/e2e/extension/config.js
@@ -25,11 +25,11 @@ e2e.ext.config = {
   /* The ECDSA pub key of the keyserver as a byte array */
   KAUTH_PUB: [4, 22, 240, 7, 228, 205, 195, 228, 101, 20, 52, 102, 64, 156, 187, 161, 103, 116, 56, 53, 184, 217, 167, 145, 0, 5, 253, 110, 141, 68, 125, 174, 234, 121, 214, 32, 3, 202, 137, 43, 233, 216, 49, 180, 106, 116, 245, 46, 53, 217, 249, 182, 122, 3, 164, 143, 107, 61, 144, 80, 103, 238, 192, 110, 54],
   /* The keyserver origin. Make sure you change this in manifest.json too */
-  TESTSERVER_ORIGIN: 'https://localhost:25519',
+  TESTSERVER_ORIGIN: 'https://keyshop.paranoids.corp.yahoo.com:25519',
   /* The name of the cookie used to authenticate users to the keyserver. */
-  AUTH_COOKIE: 'YOUR_COOKIE_HERE',
+  AUTH_COOKIE: 'YBY',
   /* Whether users need a valid auth cookie in order to use the keyserver */
-  AUTH_ENABLED: false,
+  AUTH_ENABLED: true,
   /* Some location that has access to the auth cookie. */
   AUTH_DEFAULT_ORIGIN: 'https://us-mg5.mail.yahoo.com'
 };
