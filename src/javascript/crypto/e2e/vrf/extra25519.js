5fb47531ee9ae5bbd44fc3ae745b20eed9443a8b
adon <adon@yahoo-inc.com>
2015-09-16 14:25:26 +0800

    updated according to reviewer's comments

--- src/javascript/crypto/e2e/vrf/extra25519.js
+++ src/javascript/crypto/e2e/vrf/extra25519.js
@@ -21,69 +21,58 @@
 
 goog.provide('e2e.vrf.extra25519');
 
-goog.require('goog.asserts');
-goog.require('goog.crypt');
 goog.require('e2e.BigNum');
-goog.require('e2e.ecc.constant');
 goog.require('e2e.ecc.DomainParam');
+goog.require('e2e.ecc.Element');
 goog.require('e2e.ecc.PrimeCurve');
+goog.require('e2e.error.InvalidArgumentsError');
 
-goog.require('e2e.vrf.sha3Shake256');
-
-e2e.vrf.ed25519 = e2e.ecc.DomainParam.fromCurve(e2e.ecc.PrimeCurve.ED_25519);
-e2e.vrf.curve25519 = e2e.ecc.DomainParam.fromCurve(e2e.ecc.PrimeCurve.CURVE_25519);
+var ed25519 = e2e.ecc.DomainParam.fromCurve(e2e.ecc.PrimeCurve.ED_25519);
+var curve25519 = e2e.ecc.DomainParam.fromCurve(e2e.ecc.PrimeCurve.CURVE_25519);
 
 
 /**
- * Unmarshals an elliptic curve point, and validates that it is an Ed25519 public key
- * Refer to Definition 1 in https://www.iacr.org/archive/pkc2003/25670211/25670211.pdf
- * 
+ * Unmarshals an elliptic curve point, and validates that it is an Ed25519
+ *  public key
+ *
+ * Refer to Definition 1 in
+ *  https://www.iacr.org/archive/pkc2003/25670211/25670211.pdf
+ *
  * @param {!e2e.ByteArray} sBytes A byte array of length 32
  * @return {!e2e.ecc.point.Ed25519}
  */
 e2e.vrf.extra25519.fromBytesBaseGroup = function(sBytes) {
 
-	var P = e2e.vrf.ed25519.curve.pointFromByteArray(sBytes);
+  var P = ed25519.curve.pointFromByteArray(sBytes);
 
-	goog.asserts.assert(!P.isInfinity(),
-	// goog.asserts.assert(!P.isIdentity(),
-        'P != infinity');
+  if (P.isIdentity()) {
+    throw new e2e.error.InvalidArgumentsError(
+        'fromBytesBaseGroup: P != infinity');
+  }
 
-	goog.asserts.assert(P.isOnCurve(),
-        'P must satisfy the defining equation of the elliptic curve E');
+  if (!P.isOnCurve()) {
+    throw new e2e.error.InvalidArgumentsError(
+        'fromBytesBaseGroup: P != infinity');
+  }
 
-	// TODO: confirm https://github.com/google/end-to-end/issues/340
-	
-	// nP = infinity, where n is the order 
-	// goog.asserts.assert(P.multiply(e2e.vrf.ed25519.n).isInfinity(), 
-	// goog.asserts.assert(P.multiply(e2e.vrf.ed25519.n).isIdentity(),
-		// 'nP = infinity, where n is the order');
+  if (!P.multiply(ed25519.n).isIdentity()) {
+    throw new e2e.error.InvalidArgumentsError(
+        'fromBytesBaseGroup: nP = infinity, where n is the order');
+  }
 
-	return P;
-}
-
-/**
- * Returns a*A + b*B, where B is the Ed25519 base point
- *
- * @param {!e2e.BigNum} a the representative coordinate
- * @param {!e2e.ecc.Element} A the representative coordinate
- * @param {!e2e.BigNum} b the representative coordinate
- * @return {!e2e.ecc.Element}
- */
-e2e.vrf.extra25519.geDoubleScalarMultVartime = function(a, A, b) {
-	return A.multiply(a).add(e2e.vrf.ed25519.curve.B.multiply(b));
-}
-
+  return P;
+};
 
 // converts a uniform representative value for a
 // curve25519 public key, as produced by ScalarBaseMult, to a curve25519 public
 // key.
 // See Elligator paper section 5.2.
 
+
 /**
- * Converts a uniform representative value to an element in Curve25519, 
+ * Converts a uniform representative value to an element in Curve25519,
  *  using the elligator map
- * Returns an element that is either -A/(1+ur^2) or A - A/(1+ur^2), 
+ * Returns an element that is either -A/(1+ur^2) or A - A/(1+ur^2),
  *  where u = 2, A = 486662 (defined in curve25519)
  *
  * Refer to Section 5.2 in http://elligator.cr.yp.to/elligator-20130828.pdf
@@ -91,58 +80,60 @@ e2e.vrf.extra25519.geDoubleScalarMultVartime = function(a, A, b) {
  * @return {!e2e.ecc.Element}
  */
 function representativeToMontgomeryX(r) {
-	var A = e2e.vrf.curve25519.curve.A;
+  var A = curve25519.curve.A;
 
-	// v = -A/(1+ur^2), where u = 2 (any non-square field element)
-	r = r.square();
-	var v = r.add(r).add(e2e.vrf.ed25519.curve.ONE).inverse().multiply(A).negate();
+  // v = -A/(1+ur^2), where u = 2 (any non-square field element)
+  r = r.square();
+  var v = r.add(r).add(ed25519.curve.ONE).inverse().multiply(A).negate();
 
-	// assertEquals('d4ad43e1aaf9b0ce31093a2cbe62af7e53bcb072c804e23b0d395147be6eed44',
-	// 	goog.crypt.byteArrayToHex(v.x.toByteArray().reverse()));
+  // assertEquals('d4ad43e1aaf9b0ce31093a2cbe62af7e53bcb072c804e23b0d395147be6eed44',
+  //  goog.crypt.byteArrayToHex(v.x.toByteArray().reverse()));
 
-	// e = Chi(v^3 + Av^2 + Bv), where B = 1
-	// Chi(z) = z ^ (q - 1) / 2
-	var v2 = v.square();
-	var v3 = v2.multiply(v);
-	var e = v3.add(v2.multiply(A)).add(v)
-			.power(e2e.vrf.ed25519.curve.q.subtract(e2e.BigNum.ONE).shiftRight(1));
+  // e = Chi(v^3 + Av^2 + Bv), where B = 1
+  // Chi(z) = z ^ (q - 1) / 2
+  var v2 = v.square();
+  var v3 = v2.multiply(v);
+  var e = v3.add(v2.multiply(A)).add(v)
+      .power(ed25519.curve.q.subtract(e2e.BigNum.ONE).shiftRight(1));
 
-	// leading zeros are dropped
-	// assertEquals('01',
-	// 	goog.crypt.byteArrayToHex(e.x.toByteArray().reverse()));
+  // leading zeros are dropped
+  // assertEquals('01',
+  //  goog.crypt.byteArrayToHex(e.x.toByteArray().reverse()));
 
-	// e is either 1 or -1
-	// x = ev - (1-e)A/2
-	var x = e.isEqual(e2e.vrf.ed25519.curve.ONE) ? v : A.substract(v);
+  // e is either 1 or -1
+  // x = ev - (1-e)A/2
+  // This is not constant time and thus must not be used with secret inputs.
+  var x = e.isEqual(ed25519.curve.ONE) ? v : A.substract(v);
 
-	// assertEquals('d4ad43e1aaf9b0ce31093a2cbe62af7e53bcb072c804e23b0d395147be6eed44',
-	// 	goog.crypt.byteArrayToHex(x.x.toByteArray().reverse()));
+  // assertEquals('d4ad43e1aaf9b0ce31093a2cbe62af7e53bcb072c804e23b0d395147be6eed44',
+  //  goog.crypt.byteArrayToHex(x.x.toByteArray().reverse()));
 
-	return x;
+  return x;
 }
 
+
 /**
- * Converts a Curve25519 element to an element in Ed25519 
+ * Converts a Curve25519 element to an element in Ed25519
  *
  * Refer to p.8 in http://ed25519.cr.yp.to/ed25519-20110926.pdf
  * @param {!e2e.ecc.Element} x the Curve25519 coordinate
  * @return {!e2e.ecc.Element}
  */
 function montgomeryXToEdwardsY(x) {
-	// var t, tt edwards25519.FieldElement
-	// edwards25519.FeOne(&t)
-	// edwards25519.FeAdd(&tt, x, &t)   // u+1
-	// edwards25519.FeInvert(&tt, &tt)  // 1/(u+1)
-	// edwards25519.FeSub(&t, x, &t)    // u-1
-	// edwards25519.FeMul(out, &tt, &t) // (u-1)/(u+1)
+  // var t, tt edwards25519.FieldElement
+  // edwards25519.FeOne(&t)
+  // edwards25519.FeAdd(&tt, x, &t)   // u+1
+  // edwards25519.FeInvert(&tt, &tt)  // 1/(u+1)
+  // edwards25519.FeSub(&t, x, &t)    // u-1
+  // edwards25519.FeMul(out, &tt, &t) // (u-1)/(u+1)
 
-	var y = x.add(e2e.vrf.ed25519.curve.ONE).inverse()
-			.multiply(x.subtract(e2e.vrf.ed25519.curve.ONE));
+  var y = x.add(ed25519.curve.ONE).inverse()
+      .multiply(x.subtract(ed25519.curve.ONE));
 
-	// assertEquals('2d53cc5079f9f7e495d408c88f43c839c900fea065b38c7901a76289398e283e',
-	// 	goog.crypt.byteArrayToHex(y.x.toByteArray().reverse()));
+  // assertEquals('2d53cc5079f9f7e495d408c88f43c839c900fea065b38c7901a76289398e283e',
+  //  goog.crypt.byteArrayToHex(y.x.toByteArray().reverse()));
 
-	return y;
+  return y;
 }
 
 
@@ -150,36 +141,38 @@ function montgomeryXToEdwardsY(x) {
  * Converts an 32-byte hashed output to an Ed25519 point using Elligator
  *
  * Refer to Section 5.2 in http://elligator.cr.yp.to/elligator-20130828.pdf
- * @param {!e2e.ByteArray} p A byte array of length 32
+ * @param {!e2e.ByteArray} h A byte array of length 32
  * @return {!e2e.ecc.point.Ed25519}
  */
 e2e.vrf.extra25519.hashToEdwards = function(h) {
-	// hh := *h
-	// bit := hh[31] >> 7
-	// hh[31] &= 127
-	var parity = h[31] >>> 7;
-	h[31] &= 127;
-	
-	// edwards25519.FeFromBytes(&out.Y, &hh)
-	// Here it is unsafe to use ed25519.curve.elementFromByteArray(),
-	//  as it enforces byteArray to be within the modulus q
-	var q = e2e.vrf.ed25519.curve.q;
-  	var outY = new e2e.ecc.Element(q, 
-  				new e2e.BigNum(h.reverse()).mod(q));
-	
-	// representativeToMongomeryX(&out.X, &out.Y)
-	var outX = representativeToMontgomeryX(outY);
+  // hh := *h
+  // bit := hh[31] >> 7
+  // hh[31] &= 127
+  var parity = h[31] >>> 7;
+  h[31] &= 127;
 
-	// montgomeryXToEdwardsY(&out.Y, &out.X)
-	outY = montgomeryXToEdwardsY(outX);
+  // edwards25519.FeFromBytes(&out.Y, &hh)
+  // Here it is unsafe to use ed25519.curve.elementFromByteArray(),
+  //  as it enforces byteArray to be within the modulus q
+  var q = ed25519.curve.q;
+  var outY = new e2e.ecc.Element(q,
+      new e2e.BigNum(h.reverse()).mod(q));
 
-	// if ok := out.FromParityAndY(bit, &out.Y); !ok {
-	// 	panic("HashToEdwards: point not on curve")
-	// }
-	var out = e2e.vrf.ed25519.curve.pointFromYCoordinate_(outY, parity);
-	goog.asserts.assert(out.isOnCurve(), 
-		'HashToEdwards: point not on curve');
+  // representativeToMongomeryX(&out.X, &out.Y)
+  var outX = representativeToMontgomeryX(outY);
 
-	return out;
-}
+  // montgomeryXToEdwardsY(&out.Y, &out.X)
+  outY = montgomeryXToEdwardsY(outX);
+
+  // if ok := out.FromParityAndY(bit, &out.Y); !ok {
+  //  panic("HashToEdwards: point not on curve")
+  // }
+  var out = ed25519.curve.pointFromYCoordinate_(outY, parity);
+  if (!out.isOnCurve()) {
+    throw new e2e.error.InvalidArgumentsError(
+        'HashToEdwards: point not on curve');
+  }
+
+  return out;
+};
 
--- src/javascript/crypto/e2e/vrf/vrf.js
+++ src/javascript/crypto/e2e/vrf/vrf.js
@@ -21,134 +21,140 @@
 
 goog.provide('e2e.vrf.verify');
 
-goog.require('e2e.vrf.sha3Shake256');
-goog.require('e2e.vrf.extra25519');
-
+goog.require('e2e.BigNum');
 goog.require('e2e.ecc.DomainParam');
 goog.require('e2e.ecc.PrimeCurve');
+goog.require('e2e.vrf.extra25519');
+goog.require('e2e.vrf.sha3Shake256');
 
-var PublicKeySize = 32, 
-	// SecretKeySize = 64,
-	Size = 32,
-	intermediateSize = 32,
-	ProofSize = 32 + 32 + intermediateSize;
+var PublicKeySize = 32,
+    // SecretKeySize = 64,
+    Size = 32,
+    intermediateSize = 32,
+    ProofSize = 32 + 32 + intermediateSize;
 
 
 /**
  * Returns true iff the vrf is correctly validated
  * Refer to https://github.com/yahoo/coname/blob/master/vrf/vrf.go
  *
- * @param {!e2e.ByteArray} pk The public key, stored in a byte array of length 32
- * @param {!e2e.ByteArray} m The message, stored in a byte array of length 32
- * @param {!e2e.ByteArray} vrf The vrf, stored in a byte array of length 32
- * @param {!e2e.ByteArray} proof The proof, containing c t iiB, each consisting of 32 bytes in a byte array of length 96
+ * @param {!e2e.ByteArray} pk The public key, in a byte array of length 32
+ * @param {!e2e.ByteArray} m The message, in a byte array of length 32
+ * @param {!e2e.ByteArray} vrf The vrf, in a byte array of length 32
+ * @param {!e2e.ByteArray} proof The proof, containing c t iiB, each
+ *  consisting of 32 bytes in a byte array of length 96
  * @return {boolean}
  */
 e2e.vrf.verify = function(pk, m, vrf, proof) {
 
-	if (proof.length !== ProofSize || vrf.length !== Size || pk.length !== PublicKeySize) {
-		return false;
-	}
+  if (proof.length !== ProofSize || vrf.length !== Size ||
+      pk.length !== PublicKeySize) {
+    return false;
+  }
 
-	var ed25519 = e2e.ecc.DomainParam.fromCurve(e2e.ecc.PrimeCurve.ED_25519);
+  var ed25519 = e2e.ecc.DomainParam.fromCurve(e2e.ecc.PrimeCurve.ED_25519);
 
-	// copy(vrf[:], vrfBytes)
-	// copy(pk[:], pkBytes)
-	// copy(c[:32], proof[:32])
-	// copy(t[:32], proof[32:64])
-	// copy(iiB[:], proof[64:96])
-	var c = new e2e.BigNum(aliceProof.slice(0, 32).reverse());
-	var t = new e2e.BigNum(aliceProof.slice(32, 64).reverse());
-	var iiB = proof.slice(64, 96);
+  // copy(vrf[:], vrfBytes)
+  // copy(pk[:], pkBytes)
+  // copy(c[:32], proof[:32])
+  // copy(t[:32], proof[32:64])
+  // copy(iiB[:], proof[64:96])
+  var c = new e2e.BigNum(proof.slice(0, 32).reverse());
+  var t = new e2e.BigNum(proof.slice(32, 64).reverse());
+  var iiB = proof.slice(64, 96);
 
-	// hash := sha3.NewShake256()
-	// hash.Write(iiB[:]) // const length
-	// hash.Write(m)
-	// var hCheck [Size]byte
-	// hash.Read(hCheck[:])
-	// if !bytes.Equal(hCheck[:], vrf[:]) {
-	// 	return false
-	// }
-	// hash.Reset()
-	hash = e2e.vrf.sha3Shake256(iiB.concat(m), 32);
-	// equiv. to (vrf !== hash)
-	if (vrf.length !== hash.length || 
-		vrf.some(function(b, i){return b !== hash[i];})) {
-		return false;
-	}
+  // hash := sha3.NewShake256()
+  // hash.Write(iiB[:]) // const length
+  // hash.Write(m)
+  // var hCheck [Size]byte
+  // hash.Read(hCheck[:])
+  // if !bytes.Equal(hCheck[:], vrf[:]) {
+  //  return false
+  // }
+  // hash.Reset()
+  hash = e2e.vrf.sha3Shake256(iiB.concat(m), 32);
+  // equiv. to (vrf !== hash)
+  if (vrf.length !== hash.length ||
+      vrf.some(function(b, i) {return b !== hash[i];})) {
+    return false;
+  }
 
-	// var P, B, ii, iic edwards25519.ExtendedGroupElement
-	// var A, hmtP, iicP edwards25519.ProjectiveGroupElement
+  try {
+    // var P, B, ii, iic edwards25519.ExtendedGroupElement
+    // var A, hmtP, iicP edwards25519.ProjectiveGroupElement
 
-	// P.FromBytesBaseGroup(&pk)
-	// ii.FromBytesBaseGroup(&iiB)
-	var P = e2e.vrf.extra25519.fromBytesBaseGroup(pk),
-		ii = e2e.vrf.extra25519.fromBytesBaseGroup(iiB);
+    // P.FromBytesBaseGroup(&pk)
+    // ii.FromBytesBaseGroup(&iiB)
+    var P = e2e.vrf.extra25519.fromBytesBaseGroup(pk),
+        ii = e2e.vrf.extra25519.fromBytesBaseGroup(iiB);
 
-	// edwards25519.GeDoubleScalarMultVartime(&A, &c, &P, &t)
-	// A.ToBytes(&ABytes)
-	var ABytes = e2e.vrf.extra25519.geDoubleScalarMultVartime(c, P, t).toByteArray();
+    // edwards25519.GeDoubleScalarMultVartime(&A, &c, &P, &t)
+    // A.ToBytes(&ABytes)
+    var ABytes = P.multiply(c).add(ed25519.curve.B.multiply(t)).toByteArray();
 
-	// hm := hashToCurve(m)
-	// edwards25519.GeDoubleScalarMultVartime(&hmtP, &t, hm, &[32]byte{})
-	// edwards25519.GeDoubleScalarMultVartime(&iicP, &c, &ii, &[32]byte{})
-	var hm = hashToCurve(m);
-	var hmtP = e2e.vrf.extra25519.geDoubleScalarMultVartime(t, hm, e2e.BigNum.ZERO);
-	var iicP = e2e.vrf.extra25519.geDoubleScalarMultVartime(c, ii, e2e.BigNum.ZERO);
+    // hm := hashToCurve(m)
+    // edwards25519.GeDoubleScalarMultVartime(&hmtP, &t, hm, &[32]byte{})
+    // edwards25519.GeDoubleScalarMultVartime(&iicP, &c, &ii, &[32]byte{})
+    var hm = hashToCurve(m);
+    var zeroB = ed25519.curve.B.multiply(e2e.BigNum.ZERO);
+    var hmtP = hm.multiply(t).add(zeroB);
+    var iicP = ii.multiply(c).add(zeroB);
 
-	// iicP.ToExtended(&iic)
-	// hmtP.ToExtended(&B)
-	// edwards25519.GeAdd(&B, &B, &iic)
-	// B.ToBytes(&BBytes)
-	var BBytes = iicP.add(hmtP).toByteArray();
+    // iicP.ToExtended(&iic)
+    // hmtP.ToExtended(&B)
+    // edwards25519.GeAdd(&B, &B, &iic)
+    // B.ToBytes(&BBytes)
+    var BBytes = iicP.add(hmtP).toByteArray();
 
 
-	// var cH [64]byte
-	// hash.Write(ABytes[:]) // const length
-	// hash.Write(BBytes[:]) // const length
-	// hash.Write(m)
-	// hash.Read(cH[:])
+    // var cH [64]byte
+    // hash.Write(ABytes[:]) // const length
+    // hash.Write(BBytes[:]) // const length
+    // hash.Write(m)
+    // hash.Read(cH[:])
 
-	// the two ABytes and BBytes must be of length 32
-	// ref: e2e.ecc.point.Ed25519.prototype.toByteArray
-	hash = e2e.vrf.sha3Shake256(ABytes.concat(BBytes).concat(m), 64);
+    // the two ABytes and BBytes must be of length 32
+    // ref: e2e.ecc.point.Ed25519.prototype.toByteArray
+    hash = e2e.vrf.sha3Shake256(ABytes.concat(BBytes).concat(m), 64);
 
-	// edwards25519.ScReduce(&cRef, &cH)
-	var cRef = ed25519.n.residue(new e2e.BigNum(hash.reverse()));
+    // edwards25519.ScReduce(&cRef, &cH)
+    var cRef = ed25519.n.residue(new e2e.BigNum(hash.reverse()));
 
-	// return cRef == c
-	return cRef.isEqual(c);
-}
+    // return cRef == c
+    return cRef.isEqual(c);
+
+  } catch (e) {
+    return false;
+  }
+};
 
 
 /**
- * Returns true iff the vrf is correctly validated
- * Refer to https://github.com/yahoo/coname/blob/master/vrf/vrf.go
+ * Converts an 32-byte hashed output to an Ed25519 point using Elligator
+ * This point is multiplied by 8.
  *
- * @param {!e2e.ByteArray} pk The public key, stored in a byte array of length 32
- * @param {!e2e.ByteArray} m The message, stored in a byte array of length 32
- * @param {!e2e.ByteArray} vrf The vrf, stored in a byte array of length 32
- * @param {!e2e.ByteArray} proof The proof, containing c t iiB, each consisting of 32 bytes in a byte array of length 96
- * @return {boolean}
+ * Refer to Section 5.2 in http://elligator.cr.yp.to/elligator-20130828.pdf
+ * @param {!e2e.ByteArray} m A byte array of length 32
+ * @return {!e2e.ecc.point.Ed25519}
  */
 function hashToCurve(m) {
 
-	// H(n) = (f(h(n))^8)
-	// var hmb [32]byte
-	// sha3.ShakeSum256(hmb[:], m)
-	var hmb = e2e.vrf.sha3Shake256(m, 32);
+  // H(n) = (f(h(n))^8)
+  // var hmb [32]byte
+  // sha3.ShakeSum256(hmb[:], m)
+  var hmb = e2e.vrf.sha3Shake256(m, 32);
 
-	// var hm edwards25519.ExtendedGroupElement
-	// extra25519.HashToEdwards(&hm, &hmb)
-	// edwards25519.GeDouble(&hm, &hm)
-	// edwards25519.GeDouble(&hm, &hm)
-	// edwards25519.GeDouble(&hm, &hm)
-	// return &hm
+  // var hm edwards25519.ExtendedGroupElement
+  // extra25519.HashToEdwards(&hm, &hmb)
+  // edwards25519.GeDouble(&hm, &hm)
+  // edwards25519.GeDouble(&hm, &hm)
+  // edwards25519.GeDouble(&hm, &hm)
+  // return &hm
 
-	var hm = e2e.vrf.extra25519.hashToEdwards(hmb);
-	hm = hm.add(hm);
-	hm = hm.add(hm);
-	hm = hm.add(hm);
-	return hm;
+  var hm = e2e.vrf.extra25519.hashToEdwards(hmb);
+  hm = hm.add(hm);
+  hm = hm.add(hm);
+  hm = hm.add(hm);
+  return hm;
 }
 
